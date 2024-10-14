d13a0ee0fad08ad3943ba996930885a9f95fa0dc
imogenchesworthbell <imogen.chesworthbell@barclays.com>
2024-10-14 11:17:05 +0100

    get restaurants

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
-const Restaurant = require("../models/index")
+const Restaurant = require("../models/index");
 const db = require("../db/connection");
 
-//TODO: Create your GET Request Route Below: 
+//TODO: Create your GET Request Route Below:
 
-
-
+app.get("/restaurants", async (req, res) => {
+  const allRestaurants = await Restaurant.findAll({});
+  res.json(allRestaurants);
+});
 
 module.exports = app;
