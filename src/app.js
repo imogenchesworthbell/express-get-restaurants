1c781c32df45626e332d52fd2101b6e491e36226
imogenchesworthbell <imogen.chesworthbell@barclays.com>
2024-10-15 14:13:34 +0100

    part 2 & 3

--- src/app.js
+++ src/app.js
@@ -4,10 +4,37 @@ const Restaurant = require("../models/index");
 const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below:
+app.use(express.json());
+app.use(express.urlencoded());
 
-app.get("/restaurants", async (req, res) => {
+app.get("/restaurants", async (request, response) => {
   const allRestaurants = await Restaurant.findAll({});
-  res.json(allRestaurants);
+  response.json(allRestaurants);
+});
+
+app.get("/restaurants/:id", async (request, response) => {
+  const number = request.params.id;
+  const selectedRestaurant = await Restaurant.findByPk(number);
+  response.json(selectedRestaurant);
+});
+
+app.post("/restaurants", async (request, response) => {
+  const newRestaurant = await Restaurant.create(request.body);
+  response.json(newRestaurant);
+});
+
+app.put("/restaurants/:id", async (request, response) => {
+  const updatedRestaurant = await Restaurant.update(request.body, {
+    where: { id: request.params.id },
+  });
+  response.json(updatedRestaurant);
+});
+
+app.delete("/restaurants/:id", async (request, response) => {
+  const deletedRestaurant = await Restaurant.destroy({
+    where: { id: request.params.id },
+  });
+  response.json(deletedRestaurant);
 });
 
 module.exports = app;
