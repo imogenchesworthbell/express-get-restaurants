fc69536605a4f14967fb7c5bca9d1c1dc3d7f15a
imogenchesworthbell <imogen.chesworthbell@barclays.com>
2024-10-17 11:28:22 +0100

    pt 6 without tests updated

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const restaurantRouter = express.Router();
 const Restaurant = require("../models/Restaurant");
+const {check, validationResult } = require("express-validator")
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -17,11 +18,22 @@ restaurantRouter.get("/:id", async (request, response) => {
     response.json(restaurant)
 })
 
-restaurantRouter.post("/", async (request, response) => {
-    const restaurant = await Restaurant.create(request.body);
-    const newRestaurant = await Restaurant.findAll({})
-    response.json(newRestaurant)
+restaurantRouter.post("/", 
+    [check("name").not().isEmpty().trim().withMessage("Name is required."),
+    check("location").not().isEmpty().trim().withMessage("Location is required."),
+    check("cuisine").not().isEmpty().trim().withMessage("Cuisine is required.")]
+    , async (request, response) => {
+        const errors = validationResult(request)
+        if(!errors.isEmpty()){
+            response.json({error: errors.array()})
+        }else{
+        const newRestaurant = await Restaurant.create(request.body);
+        const restaurantAdded = await Restaurant.findAll({})
+        response.json(restaurantAdded)
+        }
 })
+
+  
 
 restaurantRouter.put("/:id", async (request, response) => {
     const updatedRestaurants = await Restaurant.update(request.body, {where: {id: request.params.id}});
