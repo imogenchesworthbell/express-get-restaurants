f45e44422c371d470fca7a70b6933bca45a012a8
imogenchesworthbell <imogen.chesworthbell@barclays.com>
2024-10-18 11:27:24 +0100

    final edits

--- src/app.js
+++ src/app.js
@@ -9,34 +9,34 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use("/restaurants", restaurantRouter);
 
-app.get("/restaurants", async (request, response) => {
-  const allRestaurants = await Restaurant.findAll({});
-  response.json(allRestaurants);
-});
+// app.get("/restaurants", async (request, response) => {
+//   const allRestaurants = await Restaurant.findAll({});
+//   response.json(allRestaurants);
+// });
 
-app.get("/restaurants/:id", async (request, response) => {
-  const number = request.params.id;
-  const selectedRestaurant = await Restaurant.findByPk(number);
-  response.json(selectedRestaurant);
-});
+// app.get("/restaurants/:id", async (request, response) => {
+//   const number = request.params.id;
+//   const selectedRestaurant = await Restaurant.findByPk(number);
+//   response.json(selectedRestaurant);
+// });
 
-app.post("/restaurants", async (request, response) => {
-  const newRestaurant = await Restaurant.create(request.body);
-  response.json(newRestaurant);
-});
+// app.post("/restaurants", async (request, response) => {
+//   const newRestaurant = await Restaurant.create(request.body);
+//   response.json(newRestaurant);
+// });
 
-app.put("/restaurants/:id", async (request, response) => {
-  const updatedRestaurant = await Restaurant.update(request.body, {
-    where: { id: request.params.id },
-  });
-  response.json(updatedRestaurant);
-});
+// app.put("/restaurants/:id", async (request, response) => {
+//   const updatedRestaurant = await Restaurant.update(request.body, {
+//     where: { id: request.params.id },
+//   });
+//   response.json(updatedRestaurant);
+// });
 
-app.delete("/restaurants/:id", async (request, response) => {
-  const deletedRestaurant = await Restaurant.destroy({
-    where: { id: request.params.id },
-  });
-  response.json(deletedRestaurant);
-});
+// app.delete("/restaurants/:id", async (request, response) => {
+//   const deletedRestaurant = await Restaurant.destroy({
+//     where: { id: request.params.id },
+//   });
+//   response.json(deletedRestaurant);
+// });
 
 module.exports = app;
